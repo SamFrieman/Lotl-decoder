4f1fad85030ada95cd04256d9d2e3fdd63f68d18
Sam Frieman <81392225+SamFrieman@users.noreply.github.com>
2026-01-03 10:38:25 -0500

    Update config.js
    Metadata to start

--- js/config.js
+++ js/config.js
@@ -1,1 +1,13 @@
+// Application configuration and constants
+
+// Application metadata
+const AppConfig = {
+    version: '2.0.0',
+    name: 'LotL Encoder/Decoder',
+    maxInputLength: 50000,
+    rateLimit: {
+        maxRequests: 100,
+        timeWindow: 60000 // 1 minute in milliseconds
+    }
+};
 
